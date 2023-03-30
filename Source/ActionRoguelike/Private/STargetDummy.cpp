daf8792f91f5574abf97f4c206c6af9caa04790c
doluo <doluo@riotgames.com>
2023-03-30 14:40:59 +0800

    Add Material Function for hit flash

--- Source/ActionRoguelike/Private/STargetDummy.cpp
+++ Source/ActionRoguelike/Private/STargetDummy.cpp
@@ -23,6 +23,7 @@ void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponen
 {
 	if (Delta < 0.0f)
 	{
+		UE_LOG(LogTemp, Log, TEXT("Current Time: %f"), GetWorld()->TimeSeconds);
 		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
 	}
 }

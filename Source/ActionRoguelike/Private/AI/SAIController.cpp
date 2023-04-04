63cc8d4e0e86b14d73cd90450f5fe1f4a81c41db
doluo <doluo@riotgames.com>
2023-04-04 10:48:18 +0800

    add pawn sensing to minion

--- Source/ActionRoguelike/Private/AI/SAIController.cpp
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -12,11 +12,11 @@ void ASAIController::BeginPlay()
 
 	RunBehaviorTree(BehaviorTree);
 
-	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
-	if (MyPawn)
-	{
-		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
-
-		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
-	}
+	// APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	// if (MyPawn)
+	// {
+	// 	GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+	//
+	// 	GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
+	// }
 }

aef3ff549c24633dbe6a77bfb80b5589cc3fd238
doluo <doluo@riotgames.com>
2023-04-04 22:41:17 +0800

    ai game mode base

---
+++ Source/ActionRoguelike/Private/SGameModeBase.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGameModeBase.h"
+
+#include "SAttributeComponent.h"
+#include "AI/SAICharacter.h"
+#include "EnvironmentQuery/EnvQueryManager.h"
+#include "EngineUtils.h"
+
+ASGameModeBase::ASGameModeBase()
+{
+	SpawnTimerInterval = 2.0f;
+}
+
+void ASGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	GetWorldTimerManager().SetTimer(TimerHandle_SpawnBots, this, &ASGameModeBase::SpawnBotTimerElapsed, SpawnTimerInterval, true);
+}
+
+void ASGameModeBase::SpawnBotTimerElapsed()
+{
+	UEnvQueryInstanceBlueprintWrapper* QueryInstance = UEnvQueryManager::RunEQSQuery(this, SpawnBotQuery, this, EEnvQueryRunMode::RandomBest5Pct, nullptr);
+
+	if (ensure(QueryInstance))
+	{
+		QueryInstance->GetOnQueryFinishedEvent().AddDynamic(this, &ASGameModeBase::OnQueryCompleted);
+	}
+}
+
+void ASGameModeBase::OnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus)
+{
+	if (QueryStatus != EEnvQueryStatus::Success)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Spawn bot EQS Query failed!"));
+		return;
+	}
+
+	int32 NumOfAliveBots = 0;
+	for (TActorIterator<ASAICharacter> It(GetWorld()); It; ++It)
+	{
+		ASAICharacter* Bot = *It;
+		USAttributeComponent* AttrComp = Cast<USAttributeComponent>(Bot->GetComponentByClass(USAttributeComponent::StaticClass()));
+		if (AttrComp && AttrComp->IsAlive())
+		{
+			++NumOfAliveBots;
+		}
+	}
+
+	float MaxBotCount = 10.0f;
+	if (DifficultyCurve)
+	{
+		MaxBotCount = DifficultyCurve->GetFloatValue(GetWorld()->TimeSeconds);
+	}
+
+	if (NumOfAliveBots >= MaxBotCount)
+	{
+		return;
+	}
+	
+	TArray<FVector> Locations = QueryInstance->GetResultsAsLocations();
+	if (Locations.IsValidIndex(0))
+	{
+		GetWorld()->SpawnActor<AActor>(MinionClass, Locations[0], FRotator::ZeroRotator);
+	}
+}

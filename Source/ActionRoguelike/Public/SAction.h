3c46f6cfc447224d8b0cc5332bc0d2c87c2b2401
doluo <doluo@riotgames.com>
2023-04-11 10:09:12 +0800

    test softobjectptr on icon of action

--- Source/ActionRoguelike/Public/SAction.h
+++ Source/ActionRoguelike/Public/SAction.h
@@ -31,7 +31,7 @@ class ACTIONROGUELIKE_API USAction : public UObject
 
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="UI")
-	UTexture2D* Icon;
+	TSoftObjectPtr<UTexture2D> Icon;
 	
 	UPROPERTY(Replicated)
 	USActionComponent* ActionComp;
